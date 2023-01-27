e472b5b63c296640ae8227d90c5ff13041add6f5
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-27 11:12:42 -0300

    ✨ feat: add serveless-offline plugin

--- serverless.ts
+++ serverless.ts
@@ -4,7 +4,7 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'serveless-certificate',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -18,7 +18,20 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: {},
+  functions: {
+    hello: {
+      handler: "src/functions/hello.handler",
+      events: [
+        {
+          http: {
+            path: 'hello',
+            method: 'get',
+            cors: true
+          }
+        }
+      ]
+    }
+  },
   package: { individually: true },
   custom: {
     esbuild: {
