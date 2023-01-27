ebd159fe8e0cf3f35215e42e9ed6aef8f214986d
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-01-27 11:12:58 -0300

    ✨ feat: hello world serveless

---
+++ src/functions/hello.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: 'Hellow serveless'
+    })
+  }
+}
